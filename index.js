a89bc23512429c518b3977142d55048bdaaa370b
Wenish <Wenish@users.noreply.github.com>
2022-02-21 12:55:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ var redis = require("redis");
 
 
 async function bootstrap() {
-    // Add your cache name and access key.
-    var client = redis.createClient({
-        url: `redis://${process.env.REDISCACHEHOSTNAME}:6379`,
-        password: process.env.REDISCACHEKEY
+    const redisCacheHostName = process.env.REDISCACHEHOSTNAME
+    const redisCacheKey = process.env.REDISCACHEKEY
+
+    const client = redis.createClient({
+        url: `redis://${redisCacheHostName}:6379`,
+        password: redisCacheKey,
+        database: 2
     });
     await client.connect();
-    await client.set('key', 'value')
-    const value = client.select('key')
-    console.log(value)
+    const x = await Promise.all([
+        client.set('key', 'value'),
+        client.set('key2', 'value'),
+        client.set('key3', 'value')
+    ])
+    console.log('SET', x)
+
+    const y = await Promise.all([
+        client.get('key'),
+        client.get('key2'),
+        client.get('key3')
+    ])
+    console.log('GET', y)
+
+    await client.disconnect()
 }
 
 bootstrap()
